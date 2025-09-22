4a6b6a81dd9e4934a8c0a7b7fd995d02523ed4c9
bhartikadam14-del <bharti.kadam14@gmail.com>
2025-09-22 18:24:49 +0530

    Update MyController.java

--- src/main/java/com/example/demo/MyController.java
+++ src/main/java/com/example/demo/MyController.java
@@ -5,20 +5,32 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
+
 @RestController
 public class MyController {
-	@Autowired
-	CombineServiceApplication csa;
 
-	
-@GetMapping("/{id}")
-public Combineservice show(@PathVariable int id) {
- RestTemplate template=csa.getRestTemplate();
- Product p=template.getForObject("https://ciran12481.pythonanywhere.com/product/"+id, Product.class);
- Customer c=template.getForObject("https://springfirst-bicx.onrender.com/"+id, Customer.class);
- Combineservice cs=new Combineservice();
- cs.setProduct(p);
- cs.setCustomer(c);
- return cs;
-}
+    @Autowired
+    private RestTemplate restTemplate;
+
+    @GetMapping("/combine/{id}")
+    public Combineservice show(@PathVariable int id) {
+        // Call external Product service
+        Product p = restTemplate.getForObject(
+            "https://ciran12481.pythonanywhere.com/product/" + id,
+            Product.class
+        );
+
+        // Call external Customer service
+        Customer c = restTemplate.getForObject(
+            "https://springfirst-bicx.onrender.com/" + id,
+            Customer.class
+        );
+
+        // Combine both into a single response
+        Combineservice cs = new Combineservice();
+        cs.setProduct(p);
+        cs.setCustomer(c);
+
+        return cs;
+    }
 }
